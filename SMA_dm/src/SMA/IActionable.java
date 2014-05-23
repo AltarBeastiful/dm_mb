8cd4fa0208ff77a86e964d6abf2b21a2784320db
Rémi Benoit <r3m1.benoit@gmail.com>
2014-05-24 00:42:41 +0200

    Define interface between environnement and agent.

--- SMA_dm/src/SMA/IActionable.java
+++ SMA_dm/src/SMA/IActionable.java
@@ -1,6 +1,7 @@
 package SMA;
 
-
+//Maybe remove agent parameter
 public interface IActionable {
-
+	public boolean move(Agent a, int newX, int newY);
+	public boolean pickupBox(Agent a, int x, int y);
 }
--- SMA_dm/src/SMA/IContext.java
+++ SMA_dm/src/SMA/IContext.java
@@ -1,5 +1,11 @@
 package SMA;
 
 public interface IContext {
+//	public Entity queryPosition(int x, int y);
 
+	public boolean isFree(int x, int y);
+	public boolean isObstacle(int x, int y);
+	public boolean isCaryable(int x, int y);
+	public boolean isAgent(int x, int y);
+	public boolean isAgentCarying(int x, int y);
 }

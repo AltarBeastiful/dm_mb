3bdd27e3604ac2336c4c81f234e901a7bb2b8ad4
Rémi Benoit <r3m1.benoit@gmail.com>
2014-05-24 22:11:10 +0200

    Add Clock implementation

---
+++ SMA_dm/src/framework/Tick.java
@@ -0,0 +1,5 @@
+package framework;
+
+public interface Tick {
+	public void getTick(String uid);
+}
---
+++ SMA_dm/src/framework/impl/Clock_impl.java
@@ -0,0 +1,106 @@
+package framework.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import framework.Clock;
+import framework.SpeedRegulation;
+import framework.Tick;
+
+public class Clock_impl extends Clock implements Tick, SpeedRegulation, Runnable{
+	private Map<String, Integer> agentToken;
+	private Map<String, Thread> waitingThreads;
+	
+	private ScheduledFuture<?> tickthread;
+	private int currentSpeed;
+	private boolean isFullSpeed;
+	
+	public Clock_impl() {
+		agentToken = new HashMap<String, Integer>();
+		waitingThreads = new HashMap<String, Thread>();
+		
+		currentSpeed = 300;
+		isFullSpeed = false;
+	}
+	
+	@Override
+	protected Tick make_tick() {
+		return this;
+	}
+
+	@Override
+	protected SpeedRegulation make_speed() {
+		return this;
+	}
+
+	@Override
+	public void pause() {
+		tickthread.cancel(false);
+	}
+
+	@Override
+	public void play() {
+		ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+		this.tickthread = exec.scheduleAtFixedRate(this, 0, currentSpeed, TimeUnit.MILLISECONDS);
+	}
+
+	@Override
+	public void step() {
+		run();
+	}
+
+	@Override
+	public void setSpeed(int speed) {
+		currentSpeed = speed;
+	}
+
+	@Override
+	public void setFullSpeed(boolean isFullSpeed) {
+		this.isFullSpeed = isFullSpeed;
+	}
+
+	@Override
+	public void getTick(String uid) {
+		//TODO synchronize access 
+		if(!agentToken.containsKey(uid)) {
+			agentToken.put(uid, 0);
+		}
+		
+		int tokencount = agentToken.get(uid);
+		
+		while(tokencount == 0){
+			Thread t = Thread.currentThread();
+			waitingThreads.put(uid, t);
+			try {
+				t.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			tokencount = agentToken.get(uid);
+		}
+		
+		agentToken.put(uid, tokencount - 1);
+	}
+
+	@Override
+	public void run() {
+		for (Entry<String, Integer> entry : agentToken.entrySet()) {
+		    String uid = entry.getKey();
+		    int tokencount = entry.getValue();
+		    
+		    agentToken.put(uid, tokencount + 1);
+		    
+		    if(tokencount == 0 && waitingThreads.containsKey(uid)) {
+		    	waitingThreads.get(uid).notify();
+		    	waitingThreads.remove(uid);
+		    }
+		}
+	}
+
+}

d2cda84b990c58491d118f2838f621bd377e3804
Franck Arrecot <franck.arrecot@kdab.com>
2014-05-25 19:38:28 +0200

    Correct some IActionnable useless method and move environmentImpl from robot example

--- SMA_dm/src/framework/IActionable.java
+++ SMA_dm/src/framework/IActionable.java
@@ -3,6 +3,5 @@ package framework;
 
 //Maybe remove agent parameter
 public interface IActionable {
-	public boolean move(int x, int y, int newX, int newY) throws Exception;
-	public boolean pickupBox(Agent a, int x, int y);
+	
 }
---
+++ SMA_dm/src/robotAndBoxScenario/Environnement_impl.java
@@ -0,0 +1,80 @@
+package robotAndBoxScenario;
+
+import framework.Agent;
+//package framework.impl;
+//
+//import java.awt.Point;
+//import java.util.LinkedHashMap;
+//
+//import framework.Agent;
+//import framework.Environnement;
+//import framework.IActionable;
+//import framework.IContext;
+//
+//public class Environnement_impl extends Environnement implements IContext, IActionable {
+//
+//	private int width = 100, height = 100;
+//	private LinkedHashMap<Point, TileStatus> grid;
+//	
+//	public Environnement_impl() {
+//		this.grid = new LinkedHashMap<Point, IContext.TileStatus>();
+//	}
+//	
+//	@Override
+//	protected IContext make_context() {
+//		// TODO Auto-generated method stub
+//		return this;
+//	}
+//
+//	@Override
+//	protected IActionable make_actionable() {
+//		// TODO Auto-generated method stub
+//		return this;
+//	}
+//
+//	@Override
+//	public boolean move(int x, int y, int newX, int newY) throws Exception {
+//		// TODO Auto-generated method stub
+//		Point p = new Point(newX, newY);
+//		TileStatus status = grid.get(p);
+//		
+//		if (status == null) {
+//			throw new Exception("Undefined cooridnates out of bondaries, shouldn't be in this state") ;
+//		}
+//		
+//		TileStatus originStatus = grid.get(new Point(x,y));
+//		boolean isRobot = (originStatus.equals(TileStatus.ROBOT));
+//		boolean isRobotCary = originStatus.equals(TileStatus.ROBOT_CARRY);
+//		if (isRobot == false) {
+//			throw new Exception("There is not robot to move on the given coordinates, you liar !") ;
+//		}
+//				
+//		if(isRobot && isRobotCary && status.equals(TileStatus.BOX))
+//			return false;
+//		
+//		if (status.equals(TileStatus.OBSTACLE) || status.equals(TileStatus.ROBOT) || status.equals(TileStatus.ROBOT_CARRY)) {
+//			return false;
+//		}
+//		// actually move the robot from env pov
+//		if (status.equals(TileStatus.BOX)) 
+//			grid.put(p, TileStatus.ROBOT_CARRY);
+//		else // free tile !
+//			grid.put(p, TileStatus.ROBOT);
+//		return true;	
+//	}
+//
+//	@Override
+//	public boolean pickupBox(Agent a, int x, int y) {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
+//
+//	@Override
+//	public TileStatus queryPosition(int x, int y) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//}
+//public boolean move(int x, int y, int newX, int newY) throws Exception;
+//public boolean pickupBox(Agent a, int x, int y);

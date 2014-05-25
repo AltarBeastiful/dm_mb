55d3e98cd32f583ee6b151a06dc3cb27b6fc4345
Rémi Benoit <r3m1.benoit@gmail.com>
2014-05-25 13:37:37 +0200

    Control speed of the scenario

--- SMA_dm/src/simpleScenario/SimpleScenario.java
+++ SMA_dm/src/simpleScenario/SimpleScenario.java
@@ -1,7 +1,12 @@
 package simpleScenario;
 
+import framework.Scenario;
 import framework.impl.AbstractScenario;
 
 public class SimpleScenario extends AbstractScenario {
-
+	public static void main(String[] args) {
+		Scenario.Component scenario = new SimpleScenario().newComponent();
+		
+		scenario.speed().play();
+	}
 }
